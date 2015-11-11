0b9547f133c213cae6e4852b5ac1bee7c39f04b6
MarcelRobeer <marcel_robeer@hotmail.com>
2015-11-12 00:49:02 +0100

    Refactoring, extended story sentence structure mining

--- app/statistics.py
+++ app/statistics.py
@@ -3,15 +3,19 @@ from app.helper import Helper
 class Statistics:
 	def to_stats_array(stories):
 		stats = []
+		sent_stats = []
 
 		if stories:
-			header = ['US', 'Words', 'Verbs', 'Nouns', 'NPs', 'Ind_R', 'Ind_M', 'Ind_E', 'FR_Type', 'MV_Type', 'DO_Type']
+			header = ['US_ID', 'User_Story', 'Words', 'Verbs', 'Nouns', 'NPs', 'Ind_R', 'Ind_M', 'Ind_E', 'FR_Type', 'MV_Type', 'DO_Type']
 			stats.append(header)
+			sent_header = ['US_ID', 'Role_NP', 'Role_Struct', 'Role_Struct_Detail', 'Means_NP', 'Means_Struct', 'Means_Struct_Detail', 'Ends_NP', 'Ends_Struct', 'Ends_Struct_Detail']
+			sent_stats.append(sent_header)
 
 		for us in stories:
-			stats.append([us.number, us.stats.words, us.stats.verbs, us.stats.nouns, us.stats.noun_phrases, us.stats.indicators.role, us.stats.indicators.means, us.stats.indicators.ends, us.stats.fr_type, us.stats.mv_type, us.stats.do_type])
+			stats.append([us.number, us.text, us.stats.words, us.stats.verbs, us.stats.nouns, us.stats.noun_phrases, us.stats.indicators.role, us.stats.indicators.means, us.stats.indicators.ends, us.stats.fr_type, us.stats.mv_type, us.stats.do_type])
+			sent_stats.append([us.number, Helper.text(us.stats.role.nps), Helper.text(us.stats.role.general), Helper.text(us.stats.role.detail), Helper.text(us.stats.means.nps), Helper.text(us.stats.means.general), Helper.text(us.stats.means.detail), Helper.text(us.stats.means.nps), Helper.text(us.stats.means.general), Helper.text(us.stats.means.detail)])
 
-		return stats
+		return stats, sent_stats
 
 class Counter:
 	def count(self, story):
@@ -19,6 +23,7 @@ class Counter:
 		story = self.count_nps(story)
 		story = self.count_indicators(story)
 		story = self.get_types(story)
+		story = self.get_structure(story)
 		return story
 
 	def count_basic(self, story):
@@ -56,6 +61,34 @@ class Counter:
 			story.stats.do_type = story.means.direct_object.type
 		return story
 
+	def get_structure(self, story):
+		role = []
+		means = []
+		ends = []
+
+		for token in story.data:
+			if not token in story.indicators and token.pos_ != 'PUNCT' and token.pos_ != 'SPACE' and token.pos_ != 'X':
+				if token.i < story.means.indicator[0].i:
+					role.append([token.pos_, token.tag_, token.i])
+				elif story.ends.indicator and token.i > story.ends.indicator[-1].i:
+					ends.append([token.pos_, token.tag_, token.i])
+				else:
+					means.append([token.pos_, token.tag_, token.i])
+
+		for t in role:
+			story.stats.role.general.append(t[0])
+			story.stats.role.detail.append(t[1])
+		for t in means:
+			story.stats.means.general.append(t[0])
+			story.stats.means.detail.append(t[1])
+		for t in ends:
+			story.stats.ends.general.append(t[0])
+			story.stats.ends.detail.append(t[1])
+
+		# TODO: Replace noun phrases with word 'NP'
+
+		return story
+
 
 class UserStoryStatistics:
 	def __init__(self):
@@ -66,6 +99,9 @@ class UserStoryStatistics:
 		self.mv_type = "-"
 		self.fr_type = "-"
 		self.do_type = "-"
+		self.role = Structure()
+		self.means = Structure()
+		self.ends = Structure()
 		self.indicators = IndicatorStats()
 
 
@@ -74,3 +110,9 @@ class IndicatorStats:
 		self.role = "-"
 		self.means = "-"
 		self.ends = "-"
+
+class Structure:
+	def __init__(self):
+		self.nps = []
+		self.general = []
+		self.detail = []
