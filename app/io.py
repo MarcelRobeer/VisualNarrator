1d00f59bc5d44074aa99589231c04593011592bc
MarcelRobeer <marcel_robeer@hotmail.com>
2015-12-07 17:26:14 +0100

    Added documentation for I/O

--- app/io.py
+++ app/io.py
@@ -4,10 +4,15 @@ import pandas
 
 class Reader:
 	def parse(open_file):
+		"""Parses a previously open file
+
+		:param open_file: An open file
+		:returns: List of non-empty lines
+		"""
 		with open_file:
 			lines = []
 			for line in open_file:
-				if not line.isspace():  # Don't parse empty lines...
+				if not line.isspace():
 					lines.append(line)
 			return lines
 
@@ -15,7 +20,15 @@ class Writer:
 	def __init__(self):
 		self.number = 1
 
-	def make_file(self, dirname, filename, filetype, text):
+	def make_file(self, dirname, filename, filetype, content):
+		"""Makes a file and writes to it
+
+		:param dirname: Name of the target directory
+		:param filename: File name (without extension)
+		:param filetype: Type of file
+		:param content: Content to write to file
+		:returns: Name and location of the file
+		"""
 		if not os.path.exists(dirname):
 	    		os.makedirs(dirname)
 	
@@ -30,18 +43,28 @@ class Writer:
 
 
 		if filetype == ".csv":			
-			self.writecsv(outputname, text)
+			self.writecsv(outputname, content)
 		else:
-			self.write(outputname, text)			
+			self.write(outputname, content)			
 
 		return outputname
 
 	def write(self, outputname, text):
+		"""Writes text to a file
+
+		:param outputname: Name and location of the output file
+		:param text: Text to write to the file
+		"""
 		with open(outputname, 'w') as f:
 			f.write(text)
 			f.close()
 
 	def writecsv(self, outputname, li):
+		"""Writes a list/array/Pandas DataFrame to a CSV file
+
+		:param outputname: Name and location of the output file
+		:param li: List/array/DataFrame
+		"""
 		with open(outputname, 'wt') as f:
 			if isinstance(li, pandas.core.frame.DataFrame):
 				li.to_csv(path_or_buf=f, sep=",", quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
